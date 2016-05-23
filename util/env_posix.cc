8d0fd14278064df9ccab63d3f5b08f248b65cb97
jaehoon <jhoonl89@gmail.com>
2016-05-23 21:05:03 +0900

    added 'log file to nohost'

--- util/env_posix.cc
+++ util/env_posix.cc
@@ -71,7 +71,7 @@ bool IsSstExtention(std::string name){
 	std::string ex = list.back();
 	ex = ex.substr(0, 3);
 	//printf("DEBUG:: %s\n", ex.c_str());
-	return (ex.compare("sst") == 0 || ex.compare("ldb") == 0);
+	return (ex.compare("sst") == 0 || ex.compare("ldb") == 0 || ex.compare("log") == 0);
 }
 
 extern port::Mutex mutex_nohost;
